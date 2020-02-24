53f5be1fab1bcb5ac9d03b6243d2f126962f3c93
jeremy kale padot and Zakkrey Short <jeremy@>
2020-02-24 09:30:57 -0800

    add working single riddle question/answer

--- Sphinx.cs
+++ Sphinx.cs
@@ -4,6 +4,15 @@ class Sphinx
 {
   static void Main()
   {
-    // program code goes here
+   Console.WriteLine("Whats big, red, and eats rocks?");
+   string userAnswer = Console.ReadLine();
+   if (userAnswer == ("The big, red, rock eater"))
+   {
+       Console.WriteLine("Thats Correct!");
+   }
+   else 
+   {
+       Console.WriteLine("Prepair to die! *You are eaten by the Sphinx*");
+   }
   }
 }
