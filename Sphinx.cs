16aab7ce4b2c96a36a1e1861cfac11fb873c8e63
jeremy kale padot and Zakkrey Short <zakkrey@>
2020-02-24 10:08:43 -0800

    add 3 questions with working response. not randomized yet

--- Sphinx.cs
+++ Sphinx.cs
@@ -8,11 +8,29 @@ class Sphinx
    string userAnswer = Console.ReadLine();
    if (userAnswer == ("The big, red, rock eater"))
    {
-       Console.WriteLine("Thats Correct!");
+       Console.WriteLine("Where are my hands?");
+       string userAnswer2 = Console.ReadLine();
+       if (userAnswer2 == ("In my pockets"))
+       {
+    Console.WriteLine("Whats at the end of a rainbow?");
+           string userAnswer3 = Console.ReadLine();
+           if (userAnswer3 == ("Pot of gold"))
+           {
+               Console.WriteLine("Thats correct! You dont have to die today");
+           }
+           else
+           {
+               Console.WriteLine("Prepare to die! *You are eaten by the Sphinx*");
+           }
+       }
+       else
+       {
+       Console.WriteLine("Prepare to die! *You are eaten by the Sphinx*");
+       }
    }
    else 
    {
-       Console.WriteLine("Prepair to die! *You are eaten by the Sphinx*");
+       Console.WriteLine("Prepare to die! *You are eaten by the Sphinx*");
    }
   }
 }
